761be28da4fff73b86c42e733d49c9c88b2f8fc5
AndrewwBC <andrew-hot2011@hotmail.com>
2025-02-07 15:04:36 -0300

    add test route

--- src/main/java/com/andrew/oauth2/controller/AuthController.java
+++ src/main/java/com/andrew/oauth2/controller/AuthController.java
@@ -1,19 +1,34 @@
 package com.andrew.oauth2.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Collections;
-import java.util.Map;
 
 @RestController
 public class AuthController {
 
     @GetMapping("/user")
-    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
-        System.out.println(principal);
-        return Collections.singletonMap("name", principal.getAttribute("name"));
+    public ResponseEntity<String> user(@AuthenticationPrincipal OAuth2User principal) {
+        String name = principal.getAttribute("name");
+        System.out.println(name);
+        return ResponseEntity.status(HttpStatus.OK).body(name);
     }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> user() {
+        return ResponseEntity.status(HttpStatus.OK).body("testing");
+    }
+
+    @PostMapping("/logout")
+    public void logout(HttpServletRequest request) {
+        System.out.println("logout");
+        request.getSession().invalidate();
+    }
+
 }
--- src/main/java/com/andrew/oauth2/security/SecurityConfig.java
+++ src/main/java/com/andrew/oauth2/security/SecurityConfig.java
@@ -1,26 +1,31 @@
 package com.andrew.oauth2.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    protected OAuth2LoginConfigurer<HttpSecurity> configure(HttpSecurity http) throws Exception {
-        // @formatter:off
-      return  http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(a -> a
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/user").permitAll()
+                        .requestMatchers("/logout").permitAll()
+                        .requestMatchers("test").permitAll()
                         .anyRequest().authenticated()
                 )
-                .exceptionHandling(e -> e
+                .exceptionHandling(ex -> ex
                         .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                 )
-              .oauth2Login();
-        // @formatter:on
-    }
+                .oauth2Login();
 
+        return http.build();
+    }
 }
